9128a43abf9fb5bb83f3389608bfc1824d752a23
Mohamad Husari <7osary@gmail.com>
2019-10-02 22:39:34 +0300

    Shopping application

---
+++ precache-manifest.7bdb52c54e6cfe8bd931469b3968f6ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca301e4025c09762c3bc99386ce81d23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0d763ef3d9ec4c666fd",
+    "url": "/static/css/2.d781fd4c.chunk.css"
+  },
+  {
+    "revision": "8659fa04e5264c0d4db4",
+    "url": "/static/css/main.eddd777f.chunk.css"
+  },
+  {
+    "revision": "d0d763ef3d9ec4c666fd",
+    "url": "/static/js/2.2b2cd583.chunk.js"
+  },
+  {
+    "revision": "8659fa04e5264c0d4db4",
+    "url": "/static/js/main.934db849.chunk.js"
+  },
+  {
+    "revision": "4b2205b727eac5f5bac5",
+    "url": "/static/js/runtime-main.76197a33.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
